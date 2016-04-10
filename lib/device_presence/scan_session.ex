75296c2277305f87214c7baf17d997246ece07ff
Russell Osborne <russell@burningpony.com>
2016-04-09 21:18:41 -0400

    Flatten Session file into single list of keywords.

--- lib/device_presence/scan_session.ex
+++ lib/device_presence/scan_session.ex
@@ -39,29 +39,38 @@ defmodule DevicePresence.ScanSession do
   def scan_session(file, filter) do
     relevant_lines = String.split(file) |> Enum.filter(fn(x) -> String.contains?(x, filter) end)
 
-    grouped_lines = Enum.map(relevant_lines, fn(l) -> parse_line(l) end)
-
+    lines = Enum.map(relevant_lines, fn(l) -> parse_line(l) end)
+    # TODO: This is painful... Store in Map, and then strp groupings? must be
+    # better way
+    Enum.reduce( lines, %{}, fn(l, acc) ->
+      list = Map.get(acc, l[:id]) || []
+      Map.put(acc, l[:id], Keyword.merge(list, l))
+    end) |> Map.values
   end
 
   def parse_line(line) do
     [key | value] = String.split(line, "=")
     value = List.first(value)
     case String.split(key, ".") do
-      [klass, _type, id, field] -> [{:id, id}, {:type, String.to_atom(klass)}, parse_field(field, value)]
-      [klass, id, field] -> [{:id, id}, {:type, String.to_atom(klass)}, parse_field(field, value)]
+      [klass, _type, id, field] -> [{:id, id}, parse_field(field, value)]
+      [klass, id, field] -> [{:id, id}, parse_field(field, value)]
     end
   end
 
   def parse_field(key, value) do
     key = String.to_atom(key)
     value = case key do
-      :oldnode -> List.last(String.split(value, "."))
-      :node -> List.last(String.split(value, "."))
+      :oldnode -> parse_node_value(value)
+      :node -> parse_node_value(value)
       :time -> DateTime.from_seconds(String.to_float(value), :epoch)
       :prevtime -> DateTime.from_seconds(String.to_float(value), :epoch)
       _ -> value
     end
 
     {key , value}
+  end
+
+  defp parse_node_value(node_value_string) do
+    List.last(String.split(node_value_string, "."))
   end
 end
--- test/device_presence/scan_session_test.exs
+++ test/device_presence/scan_session_test.exs
@@ -10,16 +10,16 @@ defmodule DevicePresence.ScanSessionTest do
     file = File.read!(Path.expand("test/fixtures/session.txt"))
     [head | tail] = DevicePresence.ScanSession.events(file)
 
-    assert head[:id] == "1"
-    assert head[:node] == "72610428"
-    assert List.first(tail)[:oldnode] == "726004C8"
+    assert head[:id] == "61"
+    assert head[:node] == "1039260"
+    assert List.first(tail)[:oldnode] == "1036018"
   end
 
   test "will extract timestamps out " do
     file = File.read!(Path.expand("test/fixtures/session.txt"))
     [head | tail] = DevicePresence.ScanSession.events(file) |> Enum.filter(fn(event) -> Keyword.has_key?(event, :time) end)
 
-    assert head[:id] == "1"
+    assert head[:id] == "61"
     assert "2016" == head[:time] |> Timex.format!("{YYYY}")
   end
 end
