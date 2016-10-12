692f924bc21d9af16a841e5fea73bc88ddcb9fd1
Russell Osborne <russell@burningpony.com>
2016-10-12 16:55:14 -0400

    Provide color feedback on device and collector views.

--- web/views/device_view.ex
+++ web/views/device_view.ex
@@ -1,3 +1,11 @@
 defmodule DevicePresence.DeviceView do
   use DevicePresence.Web, :view
+
+  def offline_icon(msg) do
+    if msg == "offline" do
+      content_tag(:span, "▼ #{msg}", class: "device-down")
+    else
+      content_tag(:span, "▲ #{msg}", class: "device-up")
+    end
+  end
 end
