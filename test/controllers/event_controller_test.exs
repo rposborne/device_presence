c93ed2b344540b61084e7f7d267be7fa064b535f
Russell Osborne <russell@burningpony.com>
2016-11-19 20:38:20 -0500

    Ensure tests will compile

--- test/controllers/event_controller_test.exs
+++ test/controllers/event_controller_test.exs
@@ -4,55 +4,56 @@ defmodule DevicePresence.EventControllerTest do
   alias DevicePresence.Event
   @valid_attrs %{}
   @invalid_attrs %{}
+  @device_id 1
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
   test "lists all entries on index", %{conn: conn} do
-    conn = get conn, event_path(conn, :index)
+    conn = get conn, device_api_event_path(conn, :index, @device_id)
     assert json_response(conn, 200)["data"] == []
   end
 
   test "shows chosen resource", %{conn: conn} do
     event = Repo.insert! %Event{}
-    conn = get conn, event_path(conn, :show, event)
+    conn = get conn, device_api_event_path(conn, :show, event)
     assert json_response(conn, 200)["data"] == %{"id" => event.id}
   end
 
   test "renders page not found when id is nonexistent", %{conn: conn} do
     assert_error_sent 404, fn ->
-      get conn, event_path(conn, :show, -1)
+      get conn, device_api_event_path(conn, :show, -1)
     end
   end
 
   test "creates and renders resource when data is valid", %{conn: conn} do
-    conn = post conn, event_path(conn, :create), event: @valid_attrs
+    conn = post conn, device_api_event_path(conn, :create, @device_id), event: @valid_attrs
     assert json_response(conn, 201)["data"]["id"]
     assert Repo.get_by(Event, @valid_attrs)
   end
 
   test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, event_path(conn, :create), event: @invalid_attrs
+    conn = post conn, device_api_event_path(conn, :create, @device_id), event: @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
   test "updates and renders chosen resource when data is valid", %{conn: conn} do
     event = Repo.insert! %Event{}
-    conn = put conn, event_path(conn, :update, event), event: @valid_attrs
+    conn = put conn, device_api_event_path(conn, :update, event), event: @valid_attrs
     assert json_response(conn, 200)["data"]["id"]
     assert Repo.get_by(Event, @valid_attrs)
   end
 
   test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
     event = Repo.insert! %Event{}
-    conn = put conn, event_path(conn, :update, event), event: @invalid_attrs
+    conn = put conn, device_api_event_path(conn, :update, event), event: @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
   test "deletes chosen resource", %{conn: conn} do
     event = Repo.insert! %Event{}
-    conn = delete conn, event_path(conn, :delete, event)
+    conn = delete conn, device_api_event_path(conn, :delete, event)
     assert response(conn, 204)
     refute Repo.get(Event, event.id)
   end
