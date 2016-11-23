262ec14f2af72a0ea3bb70f99f02898c7b5b04b4
Russell Osborne <russell@burningpony.com>
2016-11-22 21:25:17 -0500

    Prep for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,9 +13,17 @@ use Mix.Config
 # which you typically run after static files are built.
 config :device_presence, DevicePresence.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "localhost", port: 8081],
   cache_static_manifest: "priv/static/manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  url: [scheme: "https", host: System.get_env("HEROKU_URL"), port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
 
+# Configure your database
+config :hello_phoenix, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 # Do not print debug messages in production
 config :logger, level: :info
 
@@ -59,7 +67,3 @@ config :logger, level: :info
 # for the new static assets to be served after a hot upgrade:
 #
 #     config :device_presence, DevicePresence.Endpoint, root: "."
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
