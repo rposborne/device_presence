8d585016c269d31ca4fc8e4174121d482d2bff9b
Russell Osborne <russell@burningpony.com>
2016-11-22 21:27:50 -0500

    Missing comm:wq

--- config/prod.exs
+++ config/prod.exs
@@ -13,13 +13,13 @@ use Mix.Config
 # which you typically run after static files are built.
 config :device_presence, DevicePresence.Endpoint,
   http: [port: {:system, "PORT"}],
-  cache_static_manifest: "priv/static/manifest.json"
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   url: [scheme: "https", host: System.get_env("HEROKU_URL"), port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Configure your database
-config :hello_phoenix, HelloPhoenix.Repo,
+config :device_presence, DevicePresence.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
